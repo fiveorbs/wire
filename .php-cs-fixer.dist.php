b425a265fd7737c459e983370e05784fde231bff
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2024-11-17 16:59:32 +0100

    Fix .php-cs-fixer.dist.php imports

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,6 +3,6 @@
 declare(strict_types=1);
 
 $finder = PhpCsFixer\Finder::create()->in([__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/docs/code']);
-$config = new Conia\Development\PhpCsFixer\Config();
+$config = new FiveOrbs\Development\PhpCsFixer\Config();
 
 return $config->setFinder($finder);
